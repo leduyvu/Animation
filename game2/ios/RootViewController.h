c55a776b8aab7f9c83979ec2c71a6a73fe769735
le duy vu <le.duy.vu@framgia.com>
2013-06-08 09:20:14 +0700

    Initial Commit

---
+++ game2/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  game2AppController.h
+//  game2
+//
+//  Created by macbook_006 on 13/06/08.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
