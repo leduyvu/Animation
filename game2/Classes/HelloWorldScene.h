c56c910e3f11cf3822f24c98f405232503cb8252
le duy vu <le.duy.vu@framgia.com>
2013-06-08 17:27:14 +0700

    animation in cocos2d-x

--- game2/Classes/HelloWorldScene.h
+++ game2/Classes/HelloWorldScene.h
@@ -12,17 +12,7 @@
 #include "cocos2d.h"
 #include "Box2D.h"
 
-class PhysicsSprite : public cocos2d::CCSprite
-{
-public:
-    PhysicsSprite();
-    void setPhysicsBody(b2Body * body);
-    virtual bool isDirty(void);
-    virtual cocos2d::CCAffineTransform nodeToParentTransform(void);
-private:
-    b2Body* m_pBody;    // strong ref
-};
-
+USING_NS_CC;
 class HelloWorld : public cocos2d::CCLayer {
 public:
     ~HelloWorld();
@@ -31,16 +21,21 @@ public:
     // returns a Scene that contains the HelloWorld as the only child
     static cocos2d::CCScene* scene();
     
-    void initPhysics();
+   // void initPhysics();
     // adds a new sprite at a given coordinate
-    void addNewSpriteAtPosition(cocos2d::CCPoint p);
+    //void addNewSpriteAtPosition(cocos2d::CCPoint p);
 
-    virtual void draw();
-    virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
+    //virtual void draw();
+    //virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
     void update(float dt);
+    void run(float i);
     
 private:
+    CCSprite *leg;
+    CCSprite * body;
+    CCSprite *gau;
     b2World* world;
+    int i =0;
     cocos2d::CCTexture2D* m_pSpriteTexture; // weak ref
 };
 
